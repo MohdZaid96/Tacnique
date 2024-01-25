a1e4fe688262f360f08cd32acc93840a3b4a620b
MohdZaid96 <i.m.mdz.1996@gmail.com>
2024-01-25 23:45:57 +0530

    Navbar Done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,31 +1,35 @@
-import React,{useState} from 'react'
-import logo from "../logo.png"
-import { Link,useNavigate } from 'react-router-dom'
-import ModalWin from './ModalWin';
+import React, { useState } from "react";
+import logo from "../logo.png";
+import { Link, useNavigate } from "react-router-dom";
+import ModalWin from "./ModalWin";
 
 const Navbar = () => {
-    const navigate=useNavigate();
-    const [editBlock, setEditBlock] = useState(false); 
+  const navigate = useNavigate();
+  const [editBlock, setEditBlock] = useState(false);
 
-    const handleAdd=()=>{
-      setEditBlock(true)
-    }
+  const handleAdd = () => {
+    setEditBlock(true);
+  };
   return (
-    <div className='Nav'>
-        <div className='logo'>
-            <img src={logo} alt='Logo'/>
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand">
+         
+          <img style={{width:"100px"}}  src={logo} alt="Logo" />
+        </a>
+        <div className="d-flex">
+          <button type="button" class="btn btn-primary" onClick={handleAdd}>
+            Add User
+          </button>
         </div>
-        <div className='add'>
-            <button onClick={handleAdd}>Add</button>
-            
-        </div>
-        <ModalWin
+      </div>
+      <ModalWin
         show={editBlock}
         onHide={() => setEditBlock(false)}
         flag={true}
       />
-    </div>
-  )
-}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
